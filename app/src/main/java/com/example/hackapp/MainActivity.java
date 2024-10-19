058d4b2fe65077c5b9987f0a808a24f2dbfed1bf
maddog0223 <kamishimamadoka1@gmail.com>
2024-10-19 14:32:02 -0400

    Logins

--- app/src/main/java/com/example/hackapp/MainActivity.java
+++ app/src/main/java/com/example/hackapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.hackapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button; //manual import
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText u_input, p_input;
+     Button loginBtn;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        u_input = findViewById(R.id.username_input);
+        p_input = findViewById(R.id.password_input);
+        loginBtn = findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = u_input.getText().toString();
+                String password = p_input.getText().toString();
+
+                //login credential
+                Log.i("Login Credential",
+                        "Username:" + username +
+                                "\nPassword: " + password);
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
